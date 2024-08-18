6e52b753d86734db8e5289f75a84b76ccc78ab29
jamesatomc <jiraphonwansai2002@gmail.com>
2024-08-18 13:00:48 +0700

    chore: Update repository links in theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -24,12 +24,12 @@ const config: DocsThemeConfig = {
     </>
   ),
   project: {
-    link: 'https://github.com/kanari-network/',
+    link: 'https://github.com/jamesatomc/kanari-docs/',
   },
   chat: {
     link: 'https://discord.gg/RKzUr4fK',
   },
-  docsRepositoryBase: 'https://github.com/kanari-network/',
+  docsRepositoryBase: 'https://github.com/jamesatomc/kanari-docs/',
   footer: {
     text: (
       <span className="text-sm text-white sm:text-center dark:text-gray-400">
