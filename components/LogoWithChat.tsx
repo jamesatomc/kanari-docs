228213fc618b636874f5a40295413c7f72f67b3c
jamesatomc <jiraphonwansai2002@gmail.com>
2024-11-27 23:33:18 +0700

    feat: Move styles from counters.module.css to logoandchat.module.css and update imports in LogoWithChat component

--- components/LogoWithChat.tsx
+++ components/LogoWithChat.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal'
-import styles from './counters.module.css'
+import styles from './styles/logoandchat.module.css'
+
 
 interface Message {
   id: number;
